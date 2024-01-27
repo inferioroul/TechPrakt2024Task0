a356a05a56a7b1984115fd9bc78f2f9518b23ebb
Timur Chernoshtan <ipz223_chtyu@student.ztu.edu.ua>
2024-01-27 16:32:43 +0200

    Added ConsoleApp C#, which calculate sum of two numbers

---
+++ PracticeConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿bool f;
+int IntegerCheck(string str)
+{
+    int value;
+    do
+    {
+        Console.Write(str);
+        f = Int32.TryParse(Console.ReadLine(), out value);
+        if (!f)
+        {
+            Console.WriteLine("Enter an integer!");
+        }
+    } while(!f);
+    return value;
+}
+int a = IntegerCheck("Enter number a = ");
+int b = IntegerCheck("Enter number b = ");
+Console.WriteLine($"a + b = {a + b}");
